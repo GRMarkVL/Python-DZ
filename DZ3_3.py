4529dfd46890d3560a9e0a32b24b0d60fc1cca38
GRMarkVL <ClimatFL@gmail.com>
2022-10-11 09:14:14 +0300

    Сделал домашнее задание №4

--- DZ3_3.py
+++ DZ3_3.py
@@ -8,22 +8,22 @@ from unittest import result
 
 list = [1.1, 1.2, 3.1, 5, 10.01]
 
-newList = [] 
+newList = [] #Собираем в список дробные части значений элементов списка
 
-for i in range(len(list)): 
-    num = round((list[i] % 1), 2) 
+for i in range(len(list)): #С помощью цикла перебираем все элементы и записываем дробную часть в список
+    num = round((list[i] % 1), 2) #округляем
     newList.append(num)
 
-print(newList)  
+print(newList)  #Печатаем список для наглядности
 
-def minimum(list): 
+def minimum(list): #Функция определения минимального значения
     min = list[0]
     for i in range(1, len(list)):
-        if list[i] < min and list[i] != 0: 
+        if list[i] < min and list[i] != 0: #исключаем из вычисления минимально значения то, у которого дробная часть осутствует
             min = list[i]
     return min
 
-def maximum(list): 
+def maximum(list): #Функция определения максимального значения
     max = list[0]
     for i in range(1, len(list)):
         if list[i] > max:
